89688f567557782a506629a299e4b79f76ae7c47
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-10 15:08:09 +0100

    add log

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(function (req, res, next) {
+    console.log('url', req.url);
+    next();
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-})
+    res.send('Hello World!');
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
 
