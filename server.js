e62963d9acc3bb6585adc829a16fa90a23286be3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-10 16:51:05 +0100

    server static

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const serveIndex = require('serve-index');
 const app = express();
 
 app.use(function (req, res, next) {
-    console.log('url', req.url);
+    console.log('urlxxx', req.url);
     next();
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+const publicDir = './www';
+app.use(express.static(publicDir));
+app.use(serveIndex(publicDir, { icons: true }));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
