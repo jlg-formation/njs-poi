1c40c9227d4daa86cf31bd463dc226a5772ed688
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-11 16:03:41 +0100

    add mongo retrieve

--- ws.js
+++ ws.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const mongodb = require('mongodb');
+
+let db;
+async function start() {
+    try {
+        const client = await mongodb.connect('mongodb://localhost', { useUnifiedTopology: true });
+        db = client.db('orsys');
+    } catch (error) {
+        db = undefined;
+        console.error('db not available.');
+        process.exit(1);
+    }
+}
+start();
 
 const app = express.Router();
 module.exports = app;
@@ -6,4 +20,14 @@ module.exports = app;
 
 app.get('/now', function (req, res, next) {
     res.json({ date: new Date() });
+});
+
+app.get('/user', async function (req, res, next) {
+    if (!db) {
+        res.status(500).end();
+        return;
+    }
+    const collection = db.collection('user');
+    const users = await collection.find({lastname: /^G/}).toArray();
+    res.json(users);
 });
